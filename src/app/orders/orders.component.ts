eeaf680c69893d47b4586ea0f73cb3f17670d369
Sergio <sergio2498@mail.fresnostate.edu>
2020-04-15 15:52:02 -0700

    finish exam one

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -11,8 +11,10 @@ import { Http } from '@angular/http';
 
 export class OrdersComponent implements OnInit {
 
-  orders:Array<any> = [];
-
+  orders: Array<any> = [];
+  name: string;
+  errorMessage = '';
+  confirmMessage = '';
   constructor(
     private router: Router,
     private flexModal: FlexModalService,
@@ -22,13 +24,111 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
-    
+    this.loadDefaultOrders();
   }
 
+  calculate() {
+    const total = this.orders.reduce((inc, item, i, arr) => {
+      inc += item.price * item.quantity;
+      return inc;
+    }, 0);
+    const taxAmount = total * .1;
+    const subTotal = total - taxAmount;
+    return {
+      total: total,
+      taxAmount: taxAmount,
+      subTotal: subTotal
+    }
+  }
+
+  submit() {
+    const commaIndex = this.name.indexOf(', ');
+    console.log('this.name', this.name, 'comma index', commaIndex);
+    let error = false;
+
+
+    if (this.name === '') {
+      console.log('Name must not be empty');
+      this.errorMessage = 'Name must not be empty';
+      error = true;
+      else if (commaIndex === -1) {
+        this.errorMessage = 'Name must have comma';
+        error = true;
+      }
+    }
+    if (!error) {
+      const firstName = this.name.slice(commaIndex + 1, this.name.length);
+      const lastName = this.name.slice(0, commaIndex);
+      const fullName = firstName + ' ' + lastName;
+      const calculation = this.calculate();
+      this.confirmMessage = `Thank you for your order ${fullName}. Your sub total is ${calculation.subTotal}. Your tax amount is ${calculation.taxAmount}. Your total is ${calculation.total}`;
+      this.flexModal.openDialog('confirm-modal');
+    } else {
+      this.flexModal.openDialog('error-modal');
+    }
+  }
+
+  loadDefaultOrders() {
+    this.orders = [{
+      'pid': '1',
+      'image': 'assets/sm_hotdog.jpeg',
+      'description': 'Hot Dog',
+      'price': 5.00,
+      'quantity': 2
+    }, {
+      'pid': '2',
+      'image': 'assets/sm_hamberger.jpeg',
+      'description': 'Hamberger',
+      'price': 6.00,
+      'quantity': 1
+    }, {
+      'pid': '3',
+      'image': 'assets/sm_pizza.jpeg',
+      'description': 'Large Pizza',
+      'price': 12.00,
+      'quantity': 2
+    }];
+  }
+
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
+
+  addItem(item: string) {
+    switch (item) {
+      case 'hot dog':
+        this.orders.unshift({
+          'pid': '1',
+          'image': 'assets/sm_hotdog.jpeg',
+          'description': 'Hot Dog',
+          'price': 5.00,
+          'quantity': 0
+        });
+        break;
+      case 'hamberger':
+        this.orders.unshift({
+          'pid': '2',
+          'image': 'assets/sm_hamberger.jpeg',
+          'description': 'Hamberger',
+          'price': 6.00,
+          'quantity': 0
+        });
+        break;
+      case 'pizza':
+        this.orders.unshift({
+          'pid': '3',
+          'image': 'assets/sm_pizza.jpeg',
+          'description': 'Large Pizza',
+          'price': 12.00,
+          'quantity': 0
+        });
+        break;
+    }
+  }
   // prepare result, splice last name, first name
 
   // Calculate total and perform input validation
-  
+
   // display the order form with orders from orders.json
 
   // Clear the orders form
